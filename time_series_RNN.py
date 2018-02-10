ca52efeeb3e6cc6c389d712c993eb8a2b81eaa01
PragyanSubedi <pragyansubedi@gmail.com>
2018-02-10 20:13:44 +0545

    Wrote operations for steps

--- time_series_RNN.py
+++ time_series_RNN.py
@@ -19,11 +19,19 @@ class TimeSeriesData():
 
         # Grab a random starting point for each batch
         rand_start = np.random.rand(batch_size,1)
-        
+
         # Convert to be on time series
+        ts_start = rand_start * (self.xmax - self.xmin - (steps*self.resolution))
 
         # Create batch time series on the x axis
+        batch_ts = ts_start + np.arange(0.0,steps+1) * self.resolution
 
         # Create the Y data for the time series x axis from previous steps
+        y_batch = np.sin(batch_ts)
 
         # Formatting for RNN
+        if return_batch_ts:
+            return y_batch[:,:-1].reshape(-1,steps,1) , y_batch[:,1:].reshape(-1,steps,1), batch_ts
+        else:
+            return y_batch[:,:-1].reshape(-1,steps,1) , y_batch[:,1:].reshape(-1,steps,1)
+
